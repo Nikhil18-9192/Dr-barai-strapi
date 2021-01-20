e3e0136feb8bbc6bbc11a83f2be401e15e2b243b
Rohan Lamb <lambrohan@gmail.com>
2021-01-20 11:25:38 +0530

    diagnose-obs-templates

--- api/appointment/models/appointment.js
+++ api/appointment/models/appointment.js
@@ -1,8 +1,52 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      const clinicalNotes = data.clinicalNotes;
+      if (clinicalNotes.observations) {
+        await updateObsTemplates(clinicalNotes.observations);
+      }
+      if (clinicalNotes.diagnoses) {
+        await updateDiagTemplates(clinicalNotes.diagnoses);
+      }
+    },
+
+    async beforeUpdate(params, data) {
+      const clinicalNotes = data.clinicalNotes;
+      if (clinicalNotes.observations) {
+        await updateObsTemplates(clinicalNotes.observations);
+      }
+      if (clinicalNotes.diagnoses) {
+        await updateDiagTemplates(clinicalNotes.diagnoses);
+      }
+    },
+  },
+};
+
+const updateObsTemplates = (obs) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await strapi.services["observation-template"].create({ value: obs });
+      resolve();
+    } catch (error) {
+      resolve();
+    }
+  });
+};
+
+const updateDiagTemplates = (diag) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await strapi.services["diagnose-template"].create({ value: diag });
+      resolve();
+    } catch (error) {
+      resolve();
+    }
+  });
+};
