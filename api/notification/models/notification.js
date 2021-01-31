4b4fbb49cc6cd05579346b481f8dd562ce213a73
Rohan Lamb <lambrohan@gmail.com>
2021-01-31 11:51:13 +0530

    sms-api-integrated

--- api/notification/models/notification.js
+++ api/notification/models/notification.js
@@ -1,8 +1,72 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
+ *
  */
+const axios = require("axios").default;
+const queryString = require("query-string");
+const PROMO_KEY = `X2g9E3wlciH7RhqW`;
+const TRAN_KEY = "QUeoo2jtaF2I8JAc";
+const API_URL = "https://www.hellotext.live/vb/apikey.php?";
+const senderid = "MSGMSG";
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(result) {
+      if (process.env.NODE_ENV !== "production") return;
+      if (result.sendToAll) {
+        await sendMessageToAll(result);
+      } else {
+        await sendMessageToSelected(result);
+      }
+    },
+  },
+};
+
+const sendMessageToAll = (result) => {
+  return new Promise(async (resolve, reject) => {
+    const nums = await strapi.services.patient.find({ _limit: -1 });
+
+    const apikey = result.type === "promotion" ? PROMO_KEY : TRAN_KEY;
+
+    const params = {
+      apikey,
+      number: nums.map((n) => n.mobile),
+      message: result.message,
+      senderid,
+    };
+
+    const url =
+      API_URL + queryString.stringify(params, { arrayFormat: "comma" });
+    try {
+      await axios.get(url);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const sendMessageToSelected = (result) => {
+  return new Promise(async (resolve, reject) => {
+    const apikey = result.type === "promotion" ? PROMO_KEY : TRAN_KEY;
+    const nums = result.patients.map((n) => n.mobile);
+    const params = {
+      apikey,
+      number: nums,
+      message: result.message,
+      senderid,
+    };
+
+    const url =
+      API_URL + queryString.stringify(params, { arrayFormat: "comma" });
+    try {
+      await axios.get(url);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
