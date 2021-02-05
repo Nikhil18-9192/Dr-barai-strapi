ec9cdb14bdbcc5006fe52cc9f48af94771c0bd05
Rohan Lamb <lambrohan@gmail.com>
2021-02-05 11:07:50 +0530

    auto-sms-on-appointment

--- api/appointment/models/appointment.js
+++ api/appointment/models/appointment.js
@@ -4,7 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
-
+const moment = require("moment");
 module.exports = {
   lifecycles: {
     async beforeCreate(data) {
@@ -15,6 +15,23 @@ module.exports = {
       }
       if (clinicalNotes.diagnoses) {
         await updateDiagTemplates(clinicalNotes.diagnoses);
+      }
+    },
+
+    async afterCreate(result) {
+      const date = moment(result.startDateTime).format("DD MMMM YYYY");
+      const time = moment(result.startDateTime).format("hh:mm A");
+
+      const patients = [result.patient.id];
+
+      const message = `You have/had an appointment at Dr. Barais Clinic on ${date} ${time}. Contact +919766387420 for more info.`;
+      try {
+        await strapi.services.notification.create({
+          message,
+          patients,
+        });
+      } catch (error) {
+        console.log("error sending appointment sms");
       }
     },
 
