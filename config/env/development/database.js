513a2ed84b94dc77b53588ff96b6985dedde9950
Nikhil18-9192 <nikhilkorane08@example.com>
2021-02-18 17:34:25 +0530

    changes in  patient model

---
+++ config/env/development/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass`,
+      },
+      options: {
+        ssl: false,
+      },
+    },
+  },
+});
